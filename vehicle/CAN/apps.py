ca54966ee995a00e723e9a1be316f58cecbcadda
wzd <wzdwxjwxw@sjtu.edu.cn>
2021-05-20 20:43:59 +0800

    side columns

--- vehicle/CAN/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CanConfig(AppConfig):
-    name = 'CAN'
--- vehicle/vehicle/generalModels.py
+++
@@ -1,9 +0,0 @@
-# 最底层的数据单元
-class SingleData:
-
-    def __init__(self, can_id, time, data_in_hex, data_in_binary):
-        self.can_id = can_id
-        # 这里的time有点不太清楚
-        self.time = time
-        self.data_in_hex = data_in_hex
-        self.data_in_binary = data_in_binary
